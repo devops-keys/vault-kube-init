e45c304ebcb979b75fe2290eeba271c9fc8384e1
devops-keys <keysdevopos@gmail.com>
2020-01-25 10:58:38 +0530

    vault

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func check(e error) {
@@ -16,4 +20,26 @@ func main() {
 	dat, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/token")
 	check(err)
 	fmt.Print(string(dat))
+
+	message := map[string]interface{}{
+		"jwt":  dat,
+		"role": "demo",
+	}
+
+	bytesRepresentation, err := json.Marshal(message)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resp, err := http.Post("http://127.0.0.1:8200/v1/auth/kubernetes/login", "application/json", bytes.NewBuffer(bytesRepresentation))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var result map[string]interface{}
+
+	json.NewDecoder(resp.Body).Decode(&result)
+
+	log.Println(result)
+	log.Println(result["data"])
 }
